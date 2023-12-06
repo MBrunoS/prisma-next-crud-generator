492b74c08efdab1ece79966f6a8eda58facadcd4
Maurício Bruno <mb.bruno.s@gmail.com>
2023-12-06 11:57:15 -0300

    feat(usg): add Next.js app w/ prisma + tailwind

---
+++ packages/usage/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
+export default config
